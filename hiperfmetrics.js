aef618702fed28bc36349312825f06c43377bb1a
qanwi1970 <Bgiiltl37>
2016-12-10 17:10:32 -0700

    maybe good enough for tracking

--- hiperfmetrics.js
+++ hiperfmetrics.js
@@ -4,17 +4,20 @@
 (function() {
 
     var metricName = "";
+    var tasks = [];
+    var currentTask = {};
 
     function create( name ) {
         metricName = name;
     }
 
     function start( stepName ) {
-
+        currentTask = new {"stepName":stepName, "startTime": new Date(), "stopTime": {}};
     }
 
     function stop() {
-
+        currentTask.stopTime = new Date();
+        tasks.push(currentTask);
     }
 
     return {
